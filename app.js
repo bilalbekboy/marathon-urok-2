16f2229bddd006d8bfefffd757289cba8e0e38f1
bilalbek11 <116166630+bilalbek11@users.noreply.github.com>
2023-04-13 19:26:05 +0600

    marathon-urok-2

---
+++ app.js
@@ -0,0 +1,13 @@
+const item = document. querSelector('.item')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+
+function dragstart() {
+console.log('drag start')
+}
+
+function dragend() {
+    console.log('drag start')
+}
